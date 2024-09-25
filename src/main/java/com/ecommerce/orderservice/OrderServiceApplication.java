d5263b78c720376c636c62577b51a6c9505cf2ba
haben2024 <haben.e@kachadfs.com>
2024-09-25 11:03:35 +0300

    Adding real-time messaging, caching, and global exception handling.

--- src/main/java/com/ecommerce/orderservice/OrderServiceApplication.java
+++ src/main/java/com/ecommerce/orderservice/OrderServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class OrderserviceApplication {
+public class OrderServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderserviceApplication.class, args);
+		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/ecommerce/orderservice/controller/GraphQLOrderController.java
@@ -0,0 +1,4 @@
+package com.ecommerce.orderservice.controller;
+
+public class GraphQLOrderController {
+}
